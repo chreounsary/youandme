8bc3a365f46a1de48e55693fa4aa6731866c29cb
chreounsary <chreounsary@gmail.com>
2023-02-02 10:28:47 +0700

    updated navigation and page 404

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,4 +1,28 @@
+import Link from "next/link";
+
 const navigation = [
-  {name: "Home", path: "/"},
-  {name: "NFT", path: "/nft"},
+  {name: "Category", path: "/backend/category"},
+  {name: "User", path: "/backend/user"},
+  {name: "NFT", path: "/backend/nft"},
+  {name: "NFT mint", path: "/backend/nft_mint"},
+  {name: "Learner", path: "/backend/learner"},
+  {name: "Upgrading", path: "/backend/upgrading"},
+  {name: "NFT renting", path: "/backend/renting"},
+  {name: "Rewarding", path: "/backend/rewarding"},
+  {name: "Sale", path: "/backend/sale"}
+
 ]
+
+function index() {
+
+  return (
+    <div>
+      {navigation.map((item) => (
+        <Link href={item.path} className="text-xs uppercase py-3 font-bold block text-blueGray-800 hover:text-blueGray-500">
+          <i className="fas fa-user-circle mr-2 text-sm text-blueGray-400"></i>{item.name}
+        </Link>
+      ))}
+    </div>
+  );
+}
+export default index;
